1408f2250ceafce869da700603d16fc87453eb7f
tanluk1010@gmail.com <tanluk1010@gmail.com>
2025-04-30 12:19:59 +0300

    внесла зміни

--- Lesson 1/main.ts
+++ Lesson 1/main.ts
@@ -1,2 +1,26 @@
-let a:string ='hello okten';
-let b:number = 234;
+let a='hello'
+console.log (a);
+let b='owu'
+console.log (b);
+let c ='com'
+console.log (c);
+let d='ua'
+console.log (d);
+let e=1
+console.log (e);
+let f=10
+console.log (f);
+let h=-999
+console.log (h);
+let j=123
+console.log (j);
+let k=3.14
+console.log (k);
+let l=2.7
+console.log (l);
+let m=16
+console.log (m);
+let n=true
+console.log (n);
+let p=false
+console.log (p);
